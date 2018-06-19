5fe37d8a365ca9a031fb698b8956fd1d0f848a1c
anny <annycerverasaldana@gmail.com>
2018-06-19 17:20:09 +0200

    generación fichero txt

--- src/GenerateContestants.java
+++ src/GenerateContestants.java
@@ -5,7 +5,8 @@ import java.util.Random;
 public class GenerateContestants {
     public static void main(String[] args) {
         try (PrintWriter pw = new PrintWriter("contestants.txt")) {
-            int nContestants = 10000000;
+            // int nContestants = 10000000;
+        	int nContestants = 100;
             pw.println(nContestants);
             for (int i = 0; i < nContestants; i++) {
                 pw.println("Contestant "+(i+1)+": "+new Random().nextInt(nContestants*10));
